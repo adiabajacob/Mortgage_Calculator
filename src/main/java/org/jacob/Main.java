d5060e61e600de9002a558e2781ecc1f3ee9bda7
roczyno <adiabaroczyno9@gmail.com>
2024-10-15 15:18:46 +0000

    add: check how they classes work together

--- src/main/java/org/jacob/Main.java
+++ src/main/java/org/jacob/Main.java
@@ -1,7 +1,17 @@
 package org.jacob;
 
+import java.text.NumberFormat;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int principal = (int) InputHandler.prompt("Principal: ", 1000, 1_000_000);
+        float annualInterest = (float) InputHandler.prompt("Annual Interest Rate: ", 1, 30);
+        byte years = (byte) InputHandler.prompt("Period (Years): ", 1, 30);
+
+        var calculator = new MortgageCalculator(principal, annualInterest, years);
+        double mortgage = calculator.calculateMortgage();
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
 }
